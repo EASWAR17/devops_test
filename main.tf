be5d41220167d1e997e0ab20c1b096a2bc1a8b55
Easwar17 <easwar.janakiraman@gmail.com>
2024-09-01 07:44:28 +0530

    test

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "null" {}
-
-resource "null_resource" "install_jenkins" {
-  provisioner "local-exec" {
-    command = "powershell -Command \"choco install -y jenkins\""
-  }
-}
-
-output "jenkins_status" {
-  value = "Checked Jenkins installation status and executed installation "
+module "webserver" {
+  source = "webserver"
 }
