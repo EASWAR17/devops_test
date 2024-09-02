6841b460f40c9ba8837d56ca48c0fef3d0c281c2
Easwar17 <easwar.janakiraman@gmail.com>
2024-09-02 11:25:36 +0530

    test

--- webserver/main.tf
+++ webserver/main.tf
@@ -9,11 +9,6 @@ resource "null_resource" "apache_setup" {
     EOT
   }
 
-  # provisioner "file" {
-  #   source      = "/var/www/behance/index.html"  # Local path to your HTML file (relative to your Terraform workspace)
-  #   destination = "/var/www/html/index.html"        # Path on the web server where the HTML file should be placed
-  # }
-
   triggers = {
     always_run = "${timestamp()}"
   }
